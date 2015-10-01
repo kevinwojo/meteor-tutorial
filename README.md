1612bb234c41f84162842042cf659c620dc3e141
Kevin Wojkovich <kwojkovi@purdue.edu>
2015-09-30 20:58:13 -0400

    Initial commit for tutorial.  Will return later.

---
+++ README.md
@@ -0,0 +1,3 @@
+###Readme
+My attempt at the tutorial for Meteor JS application framework. Having issues on my Mac with mongoDB so I'll try again later. Gonna play with other things for now.
+
---
+++ simple-todos.js
@@ -0,0 +1,17 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+ // This code is only run on the client
+
+	Template.body.helpers({
+		tasks : function () { 
+			return Tasks.find({});
+		}
+	});
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
